06b4fe7037ad6828592401f52133e7934ed41fc0
Winston Li <winston_li@hotmail.com>
2015-10-12 16:38:13 +0800

    (1)add dependency check while startup (2)change URL composition for querying cassandra endpoints per kubernetes cassandra example's update

--- image/run.sh
+++ image/run.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-# Copyright 2014 The Kubernetes Authors All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 sed -i  "s/%%ip%%/$(hostname -i)/g" /etc/cassandra/conf/cassandra.yaml
 # endpoints of a k8s service may be unavailable to query via apiserver right after pod creation. 
 # wait until service's endpoint ready  
 KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-while [ $(curl -k https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/${POD_NAMESPACE}/endpoints/cassandra -H "Authorization: Bearer ${KUBE_TOKEN}" | jq -r '.subsets[0].addresses[0].ip == null') == true ]; do
-  echo "Endpoint of Cassandra Service is not ready..."
+# curl -m: max operation timeout; -Ss: hide progress meter but show error; --stderr -: redirect all writes to stdout
+RET=$(curl -m 2 -Ss --stderr - -k https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/${POD_NAMESPACE}/endpoints/cassandra -H "Authorization: Bearer ${KUBE_TOKEN}" | jq -r '.subsets[0].addresses[0].ip != null')
+while [ ${RET} != true ]; do
+  echo "Endpoint of Cassandra Service is not ready...(RET=${RET})"
   sleep 2
+  RET=$(curl -m 2 -Ss --stderr - -k https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/${POD_NAMESPACE}/endpoints/cassandra -H "Authorization: Bearer ${KUBE_TOKEN}" | jq -r '.subsets[0].addresses[0].ip != null')
 done
 echo "Endpoint(s) of Cassandra Service have been ready!" 
 
